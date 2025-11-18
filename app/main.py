1ae0395f168df9d4dbd82ebde25c5ac8e8ec489f
Faran Rizwan <faran.rizwan@student.manchester.ac.uk>
2025-11-18 22:37:18 +0000

    test read blob object

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 import sys
 import os
-
+import zlib
+import hashlib
 
 def main():
     # use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
+    # print("Logs from your program will appear here!", file=sys.stderr)
     
     command = sys.argv[1]
     if command == "init":
@@ -14,6 +15,16 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+    elif command == "cat-file" and sys.argv[2] == "-p":
+        sha = sys.argv[3]
+        filename = f".git/objects/{sha[0:2]}/{sha[2:]}"
+        with open(filename, "rb") as f:
+            compressed = f.read()
+            data = zlib.decompress(compressed)
+            null_index = data.index(b"\x00")
+            content = data[null_index + 1:]
+            print(content.decode("utf-8"), end="")
+            
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
